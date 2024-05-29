28c31e7b53e451d845f7a83a0647379d74075137
Harson-David <89421486+Harson-David@users.noreply.github.com>
2024-05-28 23:37:36 -0500

    cambio un nombre

--- src/main/java/com/mycompany/proyectofinaldetrimestreharsondelahoztps36/ProyectoFinaldeTrimestreHarsonDelahozTPS36.java
+++ src/main/java/com/mycompany/proyectofinaldetrimestreharsondelahoztps36/ProyectoFinaldeTrimestreHarsonDelahozTPS36.java
@@ -7,7 +7,7 @@ import Login.LoginInicio;
 import javax.swing.SwingUtilities;
 
 
-public class ProyectoFinalAPP {
+public class ProyectoFinaldeTrimestreHarsonDelahozTPS36 {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
